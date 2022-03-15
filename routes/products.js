dabf41e8f1e96fb7ed28298d55121aee3be2dfd2
Ana Beatriz <anabiawalker@gmail.com>
2022-03-15 16:50:48 -0300

    Adicionando dados para ediçao

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ module.exports = app => {
             if(err){
                 return res.status(400).send({ err })
             }else {
-                return res.status(200).send({ data })
+                return res.status(200).send(data)
             }
         })
     })
@@ -16,18 +16,18 @@ module.exports = app => {
             if(err){
                 return res.status(400).send({ err })
             }else {
-                return res.status(200).send({ data })
+                return res.status(200).send(data)
             }
         })
     })
 
     app.post('/produtos', (req, res)=>{
         let field = req.body
-        db.query('INSERT INTO Produtos (nome, preco, quantidade, validade, tipo_produto_id, fornecedor_id) VALUES (?, ?, ?, ?, ?, ?)', [field.nome, field.preco, field.quantidade, field.validade, field.tipo_produto_id, field.fornecedor_id], (err, data) => {
+        db.query('INSERT INTO Produtos (nome, preco, quantidade, validade, tipo_produto_id, fornecedor_id) VALUES (?, ?, ?, ?, ?, ?)', [field.nome, field.preco, field.quantidade, field.validade, field.tipo_produto_id, field.fornecedor_id], err => {
             if(err){
                 return res.status(400).send({ err })
             }else {
-                return res.status(200).send({ data })
+                return res.status(200).send({ field })
             }
         })
     })
@@ -43,7 +43,15 @@ module.exports = app => {
         })
     })
     app.put('/produtos/:id', (req, res)=>{
-        db.query(`UPDATE Produtos SET nome='${req.body.nome}' WHERE id=${req.params.id}`, (err, data) => {
+        let data = req.body
+        db.query(`UPDATE Produtos SET 
+            nome='${data.nome}', 
+            preco=${data.preco}, 
+            quantidade=${data.quantidade}, 
+            validade='${data.validade}',
+            tipo_produto_id=${data.tipo_produto_id},
+            fornecedor_id=${data.fornecedor_id}
+            WHERE id=${req.params.id}`, err => {
             if(err){
                 return res.status(400).send({ err })
             }else {
