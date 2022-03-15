67bf228e9df046656a6def4ad36b834d6f436ee0
Ana Beatriz <anabiawalker@gmail.com>
2022-03-15 16:35:42 -0300

    Adicionando metodo put e get id

--- routes/products.js
+++ routes/products.js
@@ -11,8 +11,19 @@ module.exports = app => {
             }
         })
     })
+    app.get('/produtos/:id', (req, res)=>{
+        db.query(`SELECT * FROM Produtos WHERE id=${req.params.id}`, (err, data) => {
+            if(err){
+                return res.status(400).send({ err })
+            }else {
+                return res.status(200).send({ data })
+            }
+        })
+    })
+
     app.post('/produtos', (req, res)=>{
-        db.query('INSERT INTO Produtos (nome, preco, quantidade, validade, tipo_produto_id, fornecedor_id) values (?, ?, ?, ?, ?, ?)', (err, data) => {
+        let field = req.body
+        db.query('INSERT INTO Produtos (nome, preco, quantidade, validade, tipo_produto_id, fornecedor_id) VALUES (?, ?, ?, ?, ?, ?)', [field.nome, field.preco, field.quantidade, field.validade, field.tipo_produto_id, field.fornecedor_id], (err, data) => {
             if(err){
                 return res.status(400).send({ err })
             }else {
@@ -25,7 +36,18 @@ module.exports = app => {
             if(err){
                 return res.status(400).send({ err })
             }else {
-                return res.status(200).send(req.params)
+                return res.status(200).send(
+                    `Produto de id=${req.params.id} excluído.`
+                )
+            }
+        })
+    })
+    app.put('/produtos/:id', (req, res)=>{
+        db.query(`UPDATE Produtos SET nome='${req.body.nome}' WHERE id=${req.params.id}`, (err, data) => {
+            if(err){
+                return res.status(400).send({ err })
+            }else {
+                return res.status(200).send({ data })
             }
         })
     })
