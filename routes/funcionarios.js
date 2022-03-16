1f1207c85c252acb7fb38067aa275a561325945b
Ana Beatriz <anabiawalker@gmail.com>
2022-03-16 15:24:10 -0300

    Adicionando rotas para funcionarios

---
+++ routes/funcionarios.js
@@ -0,0 +1,55 @@
+const db = require('../config/db')
+
+module.exports = app => {
+    app.get('/funcionarios', (req, res) => {
+        db.query(`SELECT * FROM Funcionarios`, (err, data) => {
+            if(err){
+                return res.status(400).send({ err })
+            }else{
+                return res.status(200).send({ data })
+            }
+        })
+    })
+    app.get('/funcionarios/:id', (req, res) => {
+        db.query(`SELECT * FROM Funcionarios WHERE id=${req.params.id}`, (err, data) => {
+            if(err){
+                return res.status(400).send({ err })
+            }else{
+                return res.status(200).send({ data })
+            }
+        })
+    })
+    app.post('/funcionarios', (req, res) => {
+        let field = req.body
+        db.query(`INSERT INTO Funcionarios (nome, cargo, telefone, email) VALUES (?, ?, ?, ?)`, [field.nome, field.cargo, field.telefone, field.email], err => {
+            if(err){
+                return res.status(400).send({ err })
+            }else{
+                return res.status(200).send(req.body)
+            }
+        })
+    })
+    app.put('/funcionarios/:id', (req, res) => {
+        let field = req.body
+        db.query(`UPDATE Funcionarios SET nome='${field.nome}',
+            cargo='${field.cargo}',
+            telefone='${field.telefone}',
+            email='${field.email}'
+            WHERE id=${req.params.id}`, err => {
+            if(err){
+                return res.status(400).send({ err })
+            }else{
+                return res.status(200).send(Object.assign(req.params, req.body))
+            }
+        })
+    })
+    app.delete('/funcionarios/:id', (req, res) => {
+        db.query(`DELETE FROM Funcionarios WHERE id=${req.params.id}`, err => {
+            if(err){
+                return res.status(400).send({ err })
+            }else{
+                return res.status(200).send({id: req.params.id})
+            }
+        })
+    })
+}
